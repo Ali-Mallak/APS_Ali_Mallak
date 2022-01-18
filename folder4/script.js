baf77ef29062e90b6b1e05234163df9454cc6f12
Ali-Mallak <ali.ali.kashmar@gmail.com>
2022-01-18 23:53:49 +0330

    done task day4

---
+++ folder4/script.js
@@ -0,0 +1,72 @@
+// alert('hello')
+// let input = prompt("what is your age :");
+// console.log(input);
+// let age = parseInt(input);
+// let birthday = 1400 - age;
+// alert(`your birthday : ${birthday}`);
+// console.log(birthday);
+
+// ============================
+
+// let number = 40;
+// if (number % 2 === 0) {
+//   console.log("even");
+// } else {
+//   console.log("odd");
+// }
+
+// =================
+
+// let password = "mohammad";
+
+// if (password.length > 6) {
+//   if (password.indexOf(" ") === -1) {
+//     console.log("your password strong");
+//   } else {
+//     console.log("empty string");
+//   }
+// } else {
+//   console.log("your password is not!!!!!!");
+// }
+
+// ===========================
+
+// let day = new Date.getDate();
+// console.log(day);
+// switch (day) {
+//   case 0:
+//     console.log("sunday");
+//     break;
+//   case 1:
+//     console.log("monday");
+//   default:
+//     console.log("not valid");
+// }
+
+// =============
+
+// let valu1 = "arad";
+// let valu2 = 20;
+
+// console.log(valu1);
+// console.log(valu2);
+
+// let valuarr = [25, "arad", true, null];
+// console.log(valuarr);
+
+// console.log(valuarr.length);
+
+// // ===================
+
+// let nameone = "arad";
+// let selectR = nameone[1];
+// console.log(selectR);
+
+// let str = "benyamin";
+// console.log(srt);
+// const arrNames = ["Mohammad", "Khashyar", "Kiyan", "Arad"];
+// arrNames[2] = "Mohsen";
+// =====================
+// let movies = ['hary pater', 'angry']
+// let select = movies[0].slice(0,5);
+// console.log(select);
